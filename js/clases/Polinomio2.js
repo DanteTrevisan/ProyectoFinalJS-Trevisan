e6c2c3106cb8cd4e41f689f8720f2a458b2d4153
Dante Trevisan <dante.trevisan248@gmail.com>
2024-09-24 11:46:43 -0300

    separadas las clases Polinomio2 y Raices y agregado la ruta del archivo 'raices' en html

--- js/clases/Polinomio2.js
+++ js/clases/Polinomio2.js
@@ -18,22 +18,3 @@ class Polinomio2{
     set b(b){this.#a = b};
     set c(c){this.#a = c};
 }
-
-class Raices{
-
-    #x1;
-    #x2;
-
-    constructor(x1,x2){
-        this.#x1 = x1;
-        this.#x2 = x2;
-    }
-
-    get x1(){return this.#x1};
-    get x2(){return this.#x2};
-
-    set x1(x1){this.#x1 = x1};
-    set x2(x2){this.#x2 = x2};
-
-    imprimirRaices(){return `x1 = ${this.#x1} ; x2 = ${this.#x2}`};
-}
